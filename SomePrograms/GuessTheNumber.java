31768ec4a0f0a70df4fad46f311c087b1bf5b2e3
Imad Rahman <imadrahman2938@outlook.com>
2022-06-07 20:36:13 +0530

    .

--- SomePrograms/GuessTheNumber.java
+++ SomePrograms/GuessTheNumber.java
@@ -1,53 +1,25 @@
-<<<<<<< HEAD:SomePrograms/GuessTheNumber.java
 package SomePrograms;
 
 import java.util.*;
 
 public class GuessTheNumber {
     public static void main(String args[]) {
-=======
-package My_own_stuff;
-import java.util.*;
-public class Guess_the_number
-{
-    public static void main()
-    {
->>>>>>> 20978579b70abcd0f4e3c7937b0c65dad86055db:Some programs/GuessTheNumber.java
         Scanner sc = new Scanner(System.in);
-        int a,b,n=0;
+        int a, b, n = 0;
         b = 100;
-        a =(int) (Math.random()*b)+1;
-        for(int i=0,c=5;i<=5;i++,c--)
-        {
-            String str="";
+        a = (int) (Math.random() * b) + 1;
+
+        for (int i = 0, c = 5; i <= 5; i++, c--) {
             System.out.println("Enter a number");
             n = sc.nextInt();
-            if(c>1)
-            {
-                str="more tries left.";
-            }
-            if(c==1)
-            {
-                str="more try left.";
-            }
-            if(c==0)
-            {
-                str="tries left!";
-            }
-            if(n<0 || n>100)
-            {
-                System.out.println("Incorrect input! Input a number that is greater or equal to 0 and lesser or equal to 100!");
-                System.exit(42);
-            }
-            if(n==a)
-            {
+            if (n == a) {
                 System.out.println("You guessed the number correct!");
                 System.exit(42);
             } else {
-                System.out.println("The number you guessed was incorrect. You have " +c+" "+str);
+                System.out.println("Sadly you didn't guess it correct. You have " + c + " more tries left!");
             }
         }
-        if(n!=a)
-        System.out.println("The correct number was "+a);
+        if (n != a)
+            System.out.println("The correct number was " + a);
     }
 }
